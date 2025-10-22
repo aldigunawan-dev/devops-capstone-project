3955e367ec99122183e7513d65a7cde86c2b9058
Aldi Gunawan <78894332+aldigunawan-dev@users.noreply.github.com>
2025-10-23 03:30:18 +0700

    fix more typos....

--- service/routes.py
+++ service/routes.py
@@ -64,7 +64,7 @@ def list_accounts():
     app.logger.info("Request to list accounts...")
     accounts = Account.all()
     results = [account.serialize() for account in accounts]
-    app.logger.info("[%s] Products returned", len(results))
+    app.logger.info("[%s] Accounts returned", len(results))
     return jsonify(results), status.HTTP_200_OK
 
 ######################################################################
@@ -75,7 +75,7 @@ def list_accounts():
 def read_account(account_id):
     """
     Read an Account
-    This endpoint will return a Product based on it's id
+    This endpoint will return a Account based on it's id
     """
     app.logger.info("Retrieve an account with id [%s]", account_id)
     account = Account.find(account_id)
@@ -107,16 +107,16 @@ def update_accounts(account_id):
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-    def delete_accounts(account_id):
-        """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
-        """
-        app.logger.info("Request to delete an Account with id: %s", account_id)
-        account = Account.find(account_id)
-        if account:
-            account.delete()
-        return "", status.HTTP_204_NO_CONTENT
+  def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
